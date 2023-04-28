55b55ce609d0e0e6adb7d756b86dc5be6f16f921
凌雅 坂下 <k022g1067@g.neec.ac.jp>
2023-04-28 10:59:57 +0900

    a

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -20,19 +20,25 @@ void GameScene::Initialize() {
 	textureHandleBG_ = TextureManager::Load("bg.jpg");
 	spriteBG_ = Sprite::Create(textureHandleBG_, {0.0});
 
-	textureHandleBG_ = TextureManager::Load("stage.jpg");
-	modelStage_ = Model::Create();
-	worldTransformStage_.Initialize();
+
 	// ビュープロジェクションの初期化
 	viewProjection_.translation_.y = 1;
 	viewProjection_.translation_.z = -6;
 	viewProjection_.Initialize();
+
+	 textureHandleStage_ = TextureManager::Load("stage.jpg");
+	modelStage_ = Model::Create();
+	worldTransformStage_.Initialize();
+	
 	//ステージの位置を変更
+	
+	
 	worldTransformStage_.translation_ = {0, -1.5f, 0};
 	worldTransformStage_.scale_ = {4.5f, 1, 40};
 	//変更行列を更新
 	worldTransformStage_.matWorld_ = MakeAffineMatrix(
-	    worldTransformStage_.scale_, worldTransformStage_.rotation_,
+	 worldTransformStage_.scale_, 
+		worldTransformStage_.rotation_,
 	    worldTransformStage_.translation_);
 	//変換行列を定数バッファに転送
 	worldTransformStage_.TransferMatrix();
@@ -69,10 +75,10 @@ void GameScene::Draw() {
 
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
-	//ステージ
-	modelStage_->Draw(worldTransformStage_,viewProjection_,textureHandleStage_);
+	// ステージ
 	/// </summary>
-
+	modelStage_->Draw(worldTransformStage_, viewProjection_, textureHandleStage_);
+	
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
 #pragma endregion
